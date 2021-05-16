7b0e2fdb818ca07bef07f9ba22ca9e130763b30d
Carlos <carlos.alfonso.montero@gmail.com>
2021-05-16 13:18:05 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,19 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
 
+  let [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (isOn ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{isOn ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  let [inCart, setInCart] = useState(false);
+
+  function handleClick() {
+    setInCart((inCart) => !inCart);
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} 
+        onClick={handleClick}>
+          {inCart ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
